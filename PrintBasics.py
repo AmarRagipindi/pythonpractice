4d648318ffc00d1fa7736bee6c890a8a13442348
Amar Ragipindi <amaranatha.ragipindi@gmail.com>
2022-04-01 22:53:25 +0530

    Print basics

---
+++ PrintBasics.py
@@ -0,0 +1,12 @@
+# simple print
+print("Hello Python!")
+
+# print more than one object
+print("Hello", "how are you?")
+
+# print a tuple
+x = ("apple", "banana", "cherry")
+print(x)
+
+# Print two messages, and specify the separator:
+print("Hello", "how are you?", sep="---")
